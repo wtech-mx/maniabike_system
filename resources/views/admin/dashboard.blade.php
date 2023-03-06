1a73173781148dc9da91e305f7e24303f0e1d2b3
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-03-06 09:45:59 -0600

    Actualizacion del dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,48 +2,111 @@
 
 @section('content')
 
-<section class="dashboard">
+<section class="dashboard" style="min-height: 900px;">
     <div class="row">
 
-        <div class="col-12">
-            <h1>¡Hola! Pablo </h1>
+
+        <div class="col-12 mt-5">
+            <h1 class="text-white">¡Hola! Pablo </h1>
         </div>
 
         <div class="row">
 
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 py-3">
                 <div class="row">
 
-                    <div class="col-10">
+                    <div class="col-9">
                         <div class="btn_bg_primario">
-                            <a class="btn btn_border_primario d-flex align-items-center">
-                                <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/engranaje.png') }}" alt="">
-                                <p class="text-center">Servicios</p>
-                            </a>
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/engranaje.png') }}" alt="">
+                            <p class="text-white d-inline-block">Servicios</p>
                         </div>
                     </div>
 
-                    <div class="col-2">
-                        <a class="btn btn-primary d-flex">
-                            +
-                        </a>
+                    <div class="col-3">
+                        <div class="d-flex justify-content-center">
+                            <div class="btn_border_primario">
+                                <img class="btn_img_icon_plus" src="{{ asset('assets/admin/img/icons/boton-circular-plus.png') }}" alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 py-3">
                 <div class="row">
-
-                    <div class="col-10">
-                        <a class="btn btn-primary d-flex">
-                            Servicios
-                        </a>
+                    <div class="col-9">
+                        <div class="btn_bg_primario">
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/biker.png') }}" alt="">
+                            <p class="text-white d-inline-block">Usuarios</p>
+                        </div>
                     </div>
 
-                    <div class="col-2">
-                        <a class="btn btn-primary d-flex">
-                            +
-                        </a>
+                    <div class="col-3">
+                        <div class="d-flex justify-content-center">
+                            <div class="btn_border_primario">
+                                <img class="btn_img_icon_plus" src="{{ asset('assets/admin/img/icons/boton-circular-plus.png') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 py-3">
+                <div class="row">
+                    <div class="col-9">
+                        <div class="btn_bg_primario">
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/recordatorio.png') }}" alt="">
+                            <p class="text-white d-inline-block">Recordatorio</p>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="d-flex justify-content-center">
+                            <div class="btn_border_primario">
+                                <img class="btn_img_icon_plus" src="{{ asset('assets/admin/img/icons/boton-circular-plus.png') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 py-3">
+                <div class="row">
+                    <div class="col-9">
+                        <div class="btn_bg_primario">
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/pie-chart.png') }}" alt="">
+                            <p class="text-white d-inline-block">Reportes</p>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="d-flex justify-content-center">
+                            <div class="btn_border_primario">
+                                <img class="btn_img_icon_plus" src="{{ asset('assets/admin/img/icons/barcode.png') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 py-3">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="btn_bg_primario">
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/bicycle.png') }}" alt="">
+                            <p class="text-white d-inline-block">Bicicletas</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 py-3">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="btn_bg_primario">
+                            <img class="btn_img_icon" src="{{ asset('assets/admin/img/icons/llantas.png') }}" alt="">
+                            <p class="text-white d-inline-block">Pedidos Taller</p>
+                        </div>
                     </div>
                 </div>
             </div>
