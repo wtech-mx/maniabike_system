713dc384304bb0007c473e2f8bd7b24bb159e203
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-03-31 16:33:31 -0600

    Intento de prueba de tamañi

--- resources/views/admin/servicios/pdf_servicio.blade.php
+++ resources/views/admin/servicios/pdf_servicio.blade.php
@@ -5,8 +5,8 @@
         <title>Document</title>
     </head>
     <body>
-        <div class="" style="width: 10px;position: absolute;top:-35px;left:-30px;padding:0;">
-            {!! DNS1D::getBarcodeHTML($taller->folio, 'C128') !!}
+        <div class="" style="position: absolute;top:-35px;left:-30px;padding:0;">
+            {!! DNS1D::getBarcodeHTML($taller->folio, 'C128',3,35) !!}
         </div>
         <p style="font-size: 14px;padding:0;position: absolute;top:-15px;left:-30px;">Folio:{{$taller->folio}} / Tel: {{$cliente->telefono}} <br> Cliente:{{$cliente->nombre}} <br> </p>
     </body>
