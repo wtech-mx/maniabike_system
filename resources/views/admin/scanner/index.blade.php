4ae34220c5eeec9417038d147924a1ae071c77cb
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-10 23:12:19 -0600

    Actualazcion de collapse fucnionadno manual con servicios

--- resources/views/admin/scanner/index.blade.php
+++ resources/views/admin/scanner/index.blade.php
@@ -14,8 +14,9 @@
     <div class="row">
         <div class="col-12 mt-3 mb-3">
         <h1 class="text-white text-center">¡Scanner Servicios!</h1>
+
         <div class="col-12" style="padding: 0!important;">
-            <div class="d-flex justify-content-center mt-5">
+            <div class="d-flex justify-content-center mt-5 mb-5">
 
                 <a class="btn_servicio" href="#">
                     <img class="img_icon_form mt-2" src="{{ asset('assets/admin/img/icons/lupa_list.png') }}" alt="">
@@ -29,13 +30,59 @@
 
             </div>
         </div>
-        <div class="col-12">
-            <div class="content_qr">
-                <div id="reader"></div>
-                <div id="result"></div>
-            </div>
+
+        <div class="col-12 mb-3">
             <div class="container_request_qr"></div>
         </div>
+
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Camera <img src="{{ asset('assets/admin/img/icons/fotografia.png') }}" class="img_acrdion">
+                </button>
+              </h2>
+
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="col-12">
+                        <div class="content_qr">
+                            <div id="reader"></div>
+                            <div id="result"></div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                 Manual <img src="{{ asset('assets/admin/img/icons/teclado.png') }}" class="img_acrdion">
+                </button>
+              </h2>
+
+              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <form action="" id="productForm" name="productForm" class="row">
+
+                        <div class="col-12 mb-3">
+                            <label class="text-white" for="validationCustom01">Ingresa el SKU del producto</label>
+                            <input type="text" class="form-control" id="search"  name="search">
+                        </div>
+                        <div class="col-12 mb-3">
+                            <label for="validationCustom01">-</label>
+                            <p class="text-center">
+                                <a class="btn_save_scanner" type="submit" id="saveBtn" value="create" >Buscar</a>
+                                <a id="resetBtn">Resetear</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+        </div>
+
     </div>
 </section>
 
@@ -93,6 +140,47 @@ function onScanSuccess(result, decodedResult) {
 
 function onScanFailure(error) {
 }
+</script>
 
-    </script>
+<script type="text/javascript">
+    $(function () {
+        $('#saveBtn').click(function (e) {
+            e.preventDefault(); // prevenir el comportamiento por defecto del botón
+
+            var search = $('#search').val(); // obtener el valor del input de búsqueda
+
+            if (search !== '') {
+                $.ajax({
+                url : '{{ route('scanner.search') }}',
+                type: 'get',
+                dataType: 'html',
+                data: {
+                    'search': search
+                },
+                success: function (response) {
+                    $('.container_request_qr').html(response); // renderizar la respuesta en el contenedor
+                },
+                error: function (xhr) {
+                    console.log(xhr.responseText); // mostrar mensaje de error en la consola
+                }
+            });
+            }else{
+
+            }
+
+        });
+    });
+
+    $(function () {
+      $('#resetBtn').click(function (e) {
+        // Borra el contenido anterior
+        $('#search').val('');
+        $('.container_request_qr').empty();
+
+        // Realiza una nueva búsqueda
+        $('#saveBtn').trigger('click');
+      });
+    });
+
+      </script>
 @endsection
