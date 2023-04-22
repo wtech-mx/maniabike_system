1af27e33ae0494e070872742f2a7d87ede5a2d25
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-04-21 19:15:26 -0500

    CONFIG DE ETIQUETA

--- resources/views/admin/servicios/pdf_servicio.blade.php
+++ resources/views/admin/servicios/pdf_servicio.blade.php
@@ -8,6 +8,6 @@
         <div class="" style="position: absolute;top:-35px;left:-30px;padding:0;">
             {!! DNS1D::getBarcodeHTML($taller->folio, 'C128',2.5,30) !!}
         </div>
-        <p style="font-size: 14px;padding:0;position: absolute;top:-15px;left:-30px;">Folio:{{$taller->folio}} / Tel: {{$cliente->telefono}} <br> Cliente:{{$cliente->nombre}} <br> </p>
+        <p style="font-size: 13px;padding:0;position: absolute;top:-15px;left:-30px;display: inline-block;">#:{{$taller->folio}} /{{$taller->marca}} / Tel: {{$cliente->telefono}} <br> Cliente:{{$cliente->nombre}} <br> </p>
     </body>
 </html>
