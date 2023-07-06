4952e54efb7ece13be46b1355aeff3df26cc680d
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-07-06 16:02:11 -0600

    Agregando servidor

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -24,19 +24,30 @@ class PDFController extends Controller
             }
         }
 
+        $dominio = $request->getHost();
+        if($dominio == 'taller.maniabikes.com.mx'){
+            $pdf_productos = base_path('../public_html/taller/pdf');
+
+        }else{
+            $pdf_productos = public_path() . '/pdf';
+        }
+        $path = $pdf_productos;
+
         if($request->input('tipo') == 'Minorista'){
             // Generar el contenido del PDF utilizando los datos obtenidos
             $pdf = PDF::loadView('pdf.productos', ['productos' => $products]);
             // Guardar el PDF en el servidor (opcional)
+            //$pdf->save(public_path($path.'/productos.pdf'));
             $pdf->save(public_path('pdf/productos.pdf'));
-
             // Descargar o abrir el PDF en una nueva ventana
             return $pdf->stream('productos.pdf');
         }else{
             // Generar el contenido del PDF utilizando los datos obtenidos
             $pdf = PDF::loadView('pdf.prodcutos_mayoreo', ['productos' => $products]);
             // Guardar el PDF en el servidor (opcional)
+            //$pdf->save(public_path($path.'/productos_mayoreo.pdf'));
             $pdf->save(public_path('pdf/productos_mayoreo.pdf'));
+
 
             // Descargar o abrir el PDF en una nueva ventana
             return $pdf->stream('productos.pdf');
