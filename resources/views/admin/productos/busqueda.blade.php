41886799f0c803091f95e17fd6a30289cc8a30c3
verplancken <42614785+verplancken@users.noreply.github.com>
2023-07-20 13:39:21 -0600

    campos faltantes para ver producto

--- resources/views/admin/productos/busqueda.blade.php
+++ resources/views/admin/productos/busqueda.blade.php
@@ -23,7 +23,12 @@
                                 break;
                             }
                         }
-
+                        foreach ($item->meta_data as $item2) {
+                           if ($item2->key === 'clave_mayorista') {
+                               $clave_mayorista = $item2->value;
+                               break;
+                           }
+                       }
                         if (isset($item->images) && count($item->images) > 0) {
                             foreach ($item->images as $image) {
                                 $imageSrc = $image->src;
@@ -32,10 +37,23 @@
                             $imageSrc = '#';
                         }
 
+                        foreach ($item->meta_data as $item2) {
+                           if ($item2->key === 'id_proveedor') {
+                               $id_proveedor = $item2->value;
+                               break;
+                           }
+                       }
+                       foreach ($item->meta_data as $item2) {
+                           if ($item2->key === 'costo') {
+                               $costo = $item2->value;
+                               break;
+                           }
+                       }
+
                     @endphp
                     <tr class="text-white">
                         <td class="text-white text-left" style="font-size:11px;">
-                            <input type="checkbox" name="productos_seleccionados[]" value="{{ $item->sku }}">
+                            <input type="checkbox" class="checkProducto" name="productos_seleccionados[]" value="{{ $item->sku }}">
                         </td>
                         <td class="text-white text-center" style="font-size:11px;">
                             <a data-bs-toggle="modal" type="button" data-bs-target="#edit_modal_product{{ $item->id }}" style="font-size:11px;">
@@ -47,13 +65,13 @@
                         <td class="text-white text-center" style="font-size:11px;">{{ $item->sku }}</td>
                         <td class="text-white text-center" style="font-size:11px;">${{ $item->price }}.0</td>
                         <td class="text-white text-center" style="font-size:11px;">
-                            <form class="row" style="display: inline-block;margin-left: 10px;" method="POST" action="{{ route('scanner_product.delete', $item->id) }}">
+                            {{-- <form class="row" style="display: inline-block;margin-left: 10px;" method="POST" action="{{ route('scanner_product.delete', $item->id) }}">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-sm btn-danger" type="submit">
                                     <i class="fa fa-fw fa-trash"></i>
                                 </button>
-                            </form>
+                            </form> --}}
                         </td>
                     </tr>
                     <div class="modal fade" id="edit_modal_product{{ $item->id }}" tabindex="-1" aria-labelledby="edit_modal_product'.$product->id.'Label" aria-hidden="true">
@@ -64,53 +82,47 @@
                               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body row">
-                                <form class="row" method="POST" action="'.route('scanner_product.edit', $product->id).'" >
-                                    <input type="hidden" name="_token" value="'.csrf_token().'">
-                                    <input type="hidden" name="_method" value="PATCH">
-                                    <div class="col-12">
-                                        <p class="text-center">
+                                <div class="col-12">
+                                    <p class="text-center">
                                         <a href="{{$item->permalink}}" target="_blank"><img src="{{$imageSrc}}" style="width:180px;"></a>
-                                        </p>
-                                    </div>
-                                    <div class="col-12">
-                                    <label for="name" class="form-label">Nombre</label>
-                                    <input type="text" class="form-control" id="name" name="name" value="{{ $item->name }}">
-                                    </div>
-                                    <div class="col-3">
-                                    <label for="price" class="form-label">Precio</label>
-                                    <input type="number" class="form-control" id="price" name="price" value="{{ $item->price }}">
-                                    </div>
-                                    <div class="col-3">
-                                    <label for="sale_price" class="form-label">Promoción</label>
-                                    <input type="number" class="form-control" id="sale_price" name="sale_price" value="{{ $item->sale_price }}">
-                                    </div>
-                                    <div class="col-3">
-                                    <label for="sku" class="form-label">SKU</label>
-                                    <input type="text" class="form-control" id="sku" name="sku" value="{{ $item->sku }}">
-                                    </div>
-                                    <div class="col-3">
-                                    <label for="stock_quantity" class="form-label">Stock</label>
-                                    <input type="number" class="form-control" id="stock_quantity" name="stock_quantity" value="{{ $item->stock_quantity }}">
-                                    </div>
-                                    <div class="col-6">
-                                    <label for="costo" class="form-label">costo</label>
-                                    <input type="text" class="form-control" id="costo" name="costo" value="">
-                                    </div>
-                                    <div class="col-6">
-                                    <label for="nombre_del_proveedor" class="form-label">Proveedor</label>
-                                    <input type="text" class="form-control" id="nombre_del_proveedor" name="nombre_del_proveedor" value="{{ $nombre_del_proveedor }}">
-                                    </div>
-                                    <div class="col-6">
-                                    <label for="id_proveedor" class="form-label">Id Prove</label>
-                                    <input type="text" class="form-control" id="id_proveedor" name="id_proveedor" value="">
-                                    </div>
-                                    <div class="col-6">
-                                    <label for="clave_mayorista" class="form-label">Mayoreo</label>
-                                    <input type="text" class="form-control" id="clave_mayorista" name="clave_mayorista" value="">
-                                    </div>
-                                    <button id="save-btn" type="submit" class="btn btn-primary mt-2">Guardar cambios</button>
-                                    <button type="button" class="btn btn-secondary " data-bs-dismiss="modal">Cerrar</button>
-                                </form>
+                                    </p>
+                                </div>
+                                <div class="col-12">
+                                <label for="name" class="form-label">Nombre</label>
+                                <input type="text" class="form-control" id="name" name="name" value="{{ $item->name }}">
+                                </div>
+                                <div class="col-3">
+                                <label for="price" class="form-label">Precio</label>
+                                <input type="number" class="form-control" id="price" name="price" value="{{ $item->price }}">
+                                </div>
+                                <div class="col-3">
+                                <label for="sale_price" class="form-label">Promoción</label>
+                                <input type="number" class="form-control" id="sale_price" name="sale_price" value="{{ $item->sale_price }}">
+                                </div>
+                                <div class="col-3">
+                                <label for="sku" class="form-label">SKU</label>
+                                <input type="text" class="form-control" id="sku" name="sku" value="{{ $item->sku }}">
+                                </div>
+                                <div class="col-3">
+                                <label for="stock_quantity" class="form-label">Stock</label>
+                                <input type="number" class="form-control" id="stock_quantity" name="stock_quantity" value="{{ $item->stock_quantity }}">
+                                </div>
+                                <div class="col-6">
+                                <label for="costo" class="form-label">costo</label>
+                                <input type="text" class="form-control" id="costo" name="costo" value="{{ $costo }}">
+                                </div>
+                                <div class="col-6">
+                                <label for="nombre_del_proveedor" class="form-label">Proveedor</label>
+                                <input type="text" class="form-control" id="nombre_del_proveedor" name="nombre_del_proveedor" value="{{ $nombre_del_proveedor }}">
+                                </div>
+                                <div class="col-6">
+                                <label for="id_proveedor" class="form-label">Id Prove</label>
+                                <input type="text" class="form-control" id="id_proveedor" name="id_proveedor" value="{{ $id_proveedor }}">
+                                </div>
+                                <div class="col-6">
+                                <label for="clave_mayorista" class="form-label">Mayoreo</label>
+                                <input type="text" class="form-control" id="clave_mayorista" name="clave_mayorista" value="{{ $clave_mayorista }}">
+                                </div>
                             </div>
                           </div>
                         </div>
@@ -119,6 +131,7 @@
             </tbody>
         </table>
     </div>
+    <button type="button" id="seleccionarTodos">Seleccionar Todos</button>
     <button type="button" class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#generarpdf">Generar PDF</button>
 
     <div class="modal fade" id="generarpdf" tabindex="-1" aria-labelledby="generarpdfLabel" aria-hidden="true">
@@ -210,4 +223,38 @@
         });
     });
 </script>
+<!-- Asegúrate de incluir la librería de jQuery antes de este script -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    // Capturar el evento 'change' del checkbox maestro
+    $("#checkTodos").change(function() {
+      // Obtener el estado actual del checkbox maestro (marcado o no)
+      var isChecked = $(this).prop("checked");
+
+      // Marcar o desmarcar todos los checkboxes de la tabla según el estado del checkbox maestro
+      $(".checkProducto").prop("checked", isChecked);
+    });
+
+    // Capturar el evento 'click' del botón para seleccionar todos los checkboxes mediante AJAX
+    $("#seleccionarTodos").click(function() {
+      // Marcar todos los checkboxes de la tabla
+      $(".checkProducto").prop("checked", true);
+
+      // Realizar una petición AJAX para enviar los datos al servidor (opcional)
+      $.ajax({
+        type: "POST",
+        url: '{{ route('scanner_products.index') }}', // Reemplaza con la URL adecuada
+        data: $("#miFormulario").serialize(), // Enviar los datos del formulario (opcional)
+        success: function(response) {
+          // Manejar la respuesta del servidor aquí (opcional)
+        },
+        error: function(xhr, status, error) {
+          // Manejar errores en la petición AJAX aquí (opcional)
+        }
+      });
+    });
+  });
+</script>
+
 @endsection
