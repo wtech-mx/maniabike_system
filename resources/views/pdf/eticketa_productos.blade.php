ef23ab66f7ba436445cae8f99e757cbb4faf0fe8
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-07-27 14:00:18 -0600

    alinneado de etiqueta

--- resources/views/pdf/eticketa_productos.blade.php
+++ resources/views/pdf/eticketa_productos.blade.php
@@ -19,7 +19,7 @@
         <div class="" style="position: absolute;top:-30px;left:-42px;padding:0;">
             {!! DNS1D::getBarcodeHTML($products['sku'], 'C128',2.5,30) !!}
         </div>
-        <p style="font-size: 10px;padding:0;position: absolute;top:-10px;left:-41px;display: inline-block;">
+        <p style="font-size: 10px;padding:0;position: absolute;top:-10px;left:-39px;display: inline-block;">
             <strong>{{ Str::limit($products['name'], 25) }}</strong><br>
             <strong>{{ $products['sku']}}</strong> <br>
             <strong>{{ $clave_mayorista}}</strong>  <br>
