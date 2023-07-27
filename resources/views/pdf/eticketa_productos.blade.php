e5aa173f238081fb4b5f0ce679804f386b2576fc
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-07-27 13:58:41 -0600

    edicion de etiqeta

--- resources/views/pdf/eticketa_productos.blade.php
+++ resources/views/pdf/eticketa_productos.blade.php
@@ -16,13 +16,13 @@
         <title>Document</title>
     </head>
     <body>
-        <div class="" style="position: absolute;top:-30px;left:-41px;padding:0;">
+        <div class="" style="position: absolute;top:-30px;left:-42px;padding:0;">
             {!! DNS1D::getBarcodeHTML($products['sku'], 'C128',2.5,30) !!}
         </div>
-        <p style="font-size: 11px;padding:0;position: absolute;top:-10px;left:-45px;display: inline-block;">
-            {{ Str::limit($products['name'], 25) }}<br>
-            {{ $products['sku']}} <br>
-            {{ $clave_mayorista}}  <br>
+        <p style="font-size: 10px;padding:0;position: absolute;top:-10px;left:-41px;display: inline-block;">
+            <strong>{{ Str::limit($products['name'], 25) }}</strong><br>
+            <strong>{{ $products['sku']}}</strong> <br>
+            <strong>{{ $clave_mayorista}}</strong>  <br>
         </p>
     </body>
 </html>
