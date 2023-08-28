4bede42c06eec5a8d7624d978aa7c7f6be87a7dc
verplancken <42614785+verplancken@users.noreply.github.com>
2023-08-28 14:52:51 -0600

    customer

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -33,23 +33,27 @@ class NotasController extends Controller
         $notas = Notas::find($id);
         $notas_productos = ProductoNota::where('id_nota','=',$id)->get();
 
-        $page = 1;
-        $perPage = 1; // Número de productos por página que quieres obtener
+        $notas = Notas::find($id);
+
         $client = new \GuzzleHttp\Client();
         $response = $client->request('GET', 'https://www.maniabikes.com.mx/inicio/wp-json/wc/v3/customers', [
             'auth' => ['ck_669c65e13b042664bbf29cc9dd04f86b33b8f568', 'cs_4e770f2fa9f7bc9f5aca5d9bb5c3cda3478fea9a'],
             'query' => [
                 'search' => $notas->id_client,
-                'page' => $page,
-                'per_page' => $perPage,
             ],
         ]);
-        $total = $response->getHeaderLine(config('woocommerce.header_total'));
 
         $customers = json_decode($response->getBody());
-        $customer = reset($customers);
 
-        //dd($customer->email);
+        // Find the customer that matches the ID
+        $matchingCustomer = null;
+        foreach ($customers as $customer) {
+            if ($customer->id == $notas->id_client) {
+                $customer = $customer;
+                break;
+            }
+        }
+
         if($notas->metodo_pago == 'Deudor'){
             return view('admin.recibo.recibo_deudor',compact('notas','notas_productos','customer'));
         }else{
