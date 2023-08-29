c66d873c896d63a3ac234f146c144343de855473
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-08-29 12:59:07 -0600

    Actualziacion de etiqueta

--- resources/views/pdf/eticketa_productos.blade.php
+++ resources/views/pdf/eticketa_productos.blade.php
@@ -20,13 +20,14 @@
         <title>Document</title>
     </head>
     <body>
-        <div class="" style="position: absolute;top:-30px;left:-42px;padding:0;">
-            {!! DNS1D::getBarcodeHTML($products['sku'], 'C128',2.5,30) !!}
+        <div class="" style="position: absolute;padding:0;">
+            <div style="position: relative;top:-35px;left:-37px;">
+                {!! DNS1D::getBarcodeHTML($products['sku'], 'C128',2,25) !!}
+                <p style="font-size: 9px;padding:1px;margin:0px;">{{ Str::limit($products['name'], 75) }}</p>
+                <p style="font-size: 9px;padding:1px;margin:0px;">{{ $products['sku']}}</p>
+                <p style="font-size: 9px;padding:1px;margin:0px;">{{ $formattedPrice}} / {{ $clave_mayorista}}</p>
+            </div>
         </div>
-        <p style="font-size: 10px;padding:0;position: absolute;top:-10px;left:-39px;display: inline-block;">
-            <strong>{{ Str::limit($products['name'], 25) }}</strong><br>
-            <strong>{{ $products['sku']}}</strong> <br>
-            <strong>{{ $formattedPrice}} / {{ $clave_mayorista}}</strong>  <br>
-        </p>
+
     </body>
 </html>
